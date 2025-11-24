265fef83e1a0e4b1fe767ed75fe8507150568aab
ajayw36 <ajaywadh0608@gmail.com>
2025-11-24 16:48:58 -0500

    Started implementing TextBuffer

---
+++ TextBuffer.cpp
@@ -0,0 +1,148 @@
+#include "TextBuffer.hpp"
+
+TextBuffer::TextBuffer() : data(CharList()), cursor(data.end()), 
+row(1), column(0), index(0) {}
+
+bool TextBuffer::forward() {
+    if (cursor == data.end()) {
+        return false;
+    }
+    ++cursor;
+    ++index;
+    if (cursor == data.end()) {
+        return true;
+    }
+    if (*cursor == '\n') {
+        ++row;
+        column = 0;
+    }
+    else{
+        ++column;
+    }
+    return true;
+}
+
+bool TextBuffer::backward() {
+    if (cursor == data.begin()) {
+        return false;
+    }
+    --cursor;
+    --index;
+    if (*cursor == '\n') {
+        --row;
+        column = compute_column();
+    }
+    else{
+        --column;
+    }
+    return true;
+}
+
+void TextBuffer::insert(char c) {
+    data.insert(cursor, c);
+    ++index;
+    if (c == '\n') {
+        ++row;
+        column = 0;
+    }
+    else {
+        ++column;
+    }
+}
+
+bool TextBuffer::remove() {
+    if (cursor == data.end()) {
+        return false;
+    }
+    if (*cursor == '\n') {
+        column = compute_column();
+        --row;
+    }
+    cursor = data.erase(cursor);
+    --index;
+
+    return true;
+}
+
+void TextBuffer::move_to_row_start() {
+    while(column > 0) {
+        backward();
+    }
+}
+
+void TextBuffer::move_to_row_end() {
+    while (cursor != data.end() && *cursor != '\n') {
+        forward();
+    }
+}
+
+void TextBuffer::move_to_column(int new_column) {
+    if (column == new_column) {
+        return;
+    }
+    move_to_row_start();
+    while (column != new_column && cursor != data.end() && *cursor != '\n') {
+        forward();
+    }
+}
+
+bool TextBuffer::up() {
+    if (row == 1) {
+        return false;
+    }    
+    int temp = column;
+    move_to_row_start();
+    backward();
+    move_to_column(temp);
+    return true;
+}
+
+bool TextBuffer::down() {
+    int temp = column;
+    move_to_row_end();
+    if (cursor == data.end()) {
+        move_to_column(temp);
+        return false;
+    }
+    else {
+        forward();
+        move_to_column(temp);
+        return true;
+    }
+}
+
+bool TextBuffer::is_at_end() const {
+    return cursor == data.end();
+}
+
+char TextBuffer::data_at_cursor() const {
+    return *cursor;
+}
+
+int TextBuffer::get_row() const {
+    return row;
+}
+
+int TextBuffer::get_column() const {
+    return column;
+}
+
+int TextBuffer::get_index() const {
+    return index;
+}
+
+int TextBuffer::size() const {
+    return data.size();
+}
+
+std::string TextBuffer::stringify() const {
+
+}
+
+int TextBuffer::compute_column() const {
+
+}
+
+
+
+
